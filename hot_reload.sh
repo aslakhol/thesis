c2231549a8379de9f7d1df6e6dfe6f84d22e2b25
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-05-15 17:16:29 +0200

    adds hotreloading

--- hot_reload.sh
+++ hot_reload.sh
@@ -1,5 +1,9 @@
 python -m http.server 8080 &
 open http://localhost:8080
-fswatch -o ./**/*.adoc | xargs -n2 -I{} asciidoctor -r asciidoctor-bibtex -a stem index.adoc
+kill $(lsof -ti tcp:8080)
+fswatch -v ./**/*.adoc index.adoc | while IFS= read -r line
+do
+     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./docinfo/local" index.adoc
+done
 
 
